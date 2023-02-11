c49cf23b6bfc64e03b2eaff7280ca56a2fdbc3fa
Héctor Ortiz <hector@worky.mx>
2023-02-11 14:39:54 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1 class="text-xl text-center">
+      Reservamos Weather Comparator
+    </h1>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
